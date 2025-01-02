995b8bf46b8cca834d82fad5b6e304bc7f61cca3
Zhudongsheng75 <59612926+Zhudongsheng75@users.noreply.github.com>
2025-01-02 15:48:17 +0800

    Update run_test.py

--- tool_llama_client/virtual_server/run_test.py
+++ tool_llama_client/virtual_server/run_test.py
@@ -10,7 +10,7 @@ data = {
     "api_name": "get_whois_batch_for_bulk_whois",
     "tool_input": '{"batch_id": 1}',
     "strip": "truncate",
-    "toolbench_key": "k7cr5HU9TmWOZFGYjsz8nNDIPAgqwiBEh64J0bMyu1XdlCVaoL",
+    "toolbench_key": "your toolbench_key from ToolBench",
 }
 headers = {
     "accept": "application/json",
